2f68626a8945f0bf3a62ba4d90df38138828bdd2
Gabriel Anzaldo <gabriel@sellry.com>
2020-11-09 23:27:41 -0300

    add apy key, base example GET fetch.

--- website/app.js
+++ website/app.js
@@ -1,5 +1,27 @@
-/* Global Variables */
+const APY_KEY = '7ea45cdb1c4fee217fbf8316ab369fd0';
+const BASE_URL = 'api.openweathermap.org';
+// example
+// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+// event listeners
+const generateButton = document.getElementById('generate');
+
+const getData = async(url = '') => {
+    const response = await fetch(url);
+    try{
+        const newData = await response.json();
+        return newData;
+    } catch(error){
+        console.log('error: ', error);
+        throw new Error(error);
+    }
+};
+
+function  handleClickEvent() {
+    console.log('click aca pa...');
+    const weatherData = getData(`https://${BASE_URL}/data/2.5/weather?q=London,uk&APPID=${APY_KEY}`);
+    weatherData.then(function(data) {
+        console.log('response: ', data);
+    });
+}
+generateButton.addEventListener('click', handleClickEvent);
