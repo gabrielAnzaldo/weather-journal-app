cd784dbe5d25fef70848e407da12e972d14bbbcd
Gabriel Anzaldo <gabriel@sellry.com>
2020-11-09 22:35:30 -0300

    set up initial server.

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-// Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
+const express = require('express');
+const app = express();
 
-// Start up an instance of app
-
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Cors for cross origin allowance
-
-// Initialize the main project folder
 app.use(express.static('website'));
 
+const cors = require('cors');
+app.use(cors());
 
-// Setup Server
+const port = 3000;
+const listenFunction = () => {
+    console.log(`starting server from: ${port}`)
+};
+
+const server = app.listen(port, listenFunction);
